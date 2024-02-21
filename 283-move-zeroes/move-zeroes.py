c12b3084954f179ee8308967f243e6228717f1f1
Will Davis <45891395+will-r-davis@users.noreply.github.com>
2024-02-21 04:39:54 -0500

    Time: 128 ms (59.10%) | Memory: 18.1 MB (88.69%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -1,28 +1,15 @@
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
-        writeIndex = i = 0
-        writeValue = nums[0]
-        while i < len(nums):
+        writeIndex = 0
+        for i in range(len(nums)):
             
-            #Iterating over nums until a non-zero value is found
-            while i < len(nums) and nums[i] == 0:
-                i += 1
-
-            #Recording next non-zero value in 'writeValue'
-            if i < len(nums):
-                writeValue = nums[i]
-
-                #Writing found non-zero value to it's final index
-                nums[writeIndex] = writeValue
+            #Checking for non-zero value, and writing it to it's correct index
+            if nums[i] != 0:
+                nums[writeIndex] = nums[i]
                 writeIndex += 1
-            i += 1
 
         #Placing zeros at end of dataful list
         while writeIndex < len(nums):
             nums[writeIndex] = 0
             writeIndex += 1
-
-        """
-        Do not return anything, modify nums in-place instead.
-        """
         
