93dc079c94f9f8b4e2ca28ed64e62bfa217b46df
Will Davis <45891395+will-r-davis@users.noreply.github.com>
2024-02-27 06:26:51 -0500

    Time: 497 ms (59.09%) | Memory: 28.7 MB (96.12%) - LeetSync

--- 1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.py
+++ 1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.py
@@ -2,12 +2,13 @@ class Solution:
     def maxOperations(self, nums: List[int], k: int) -> int:
         """
         Solution with two pointers conditionally moving inward over a sorted version of nums
+        Second submission adds second check for loop condition, paring unnecessary interations
         """
 
         nums.sort()
         i = operations = 0
         j = len(nums) - 1
-        while i < j:
+        while i < j and nums[i] < k:
             cur = nums[i] + nums[j]
             if cur == k:
                 operations += 1
