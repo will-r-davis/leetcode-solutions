6e44d85bcaad2cd61eecb0ff317dae91e98573eb
Will Davis <45891395+will-r-davis@users.noreply.github.com>
2024-03-15 04:52:51 -0400

    Time: 122 ms (80.64%) | Memory: 17.6 MB (86.93%) - LeetSync

--- 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.py
+++ 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.py
@@ -1,11 +1,17 @@
 class Solution:
     def closeStrings(self, word1: str, word2: str) -> bool:
+        """
+        This solution uses two dictionaries created for each string where keys are unique characters 
+        and values are their frequency within the word. For two words to be close, 
+        a set of their keys will be identical as will be a sorted list of their values.
+        """
+        # constructing dictionaries
         dict1 = Counter(word1)
         dict2 = Counter(word2)
-        occurences1 = sorted(list(dict1.values()))
-        occurences2 = sorted(list(dict2.values()))
-        if occurences1 != occurences2:
+        # comparing both sorted lists of occurences
+        if sorted(list(dict1.values())) != sorted(list(dict2.values())):
             return False
+        # comparing both sets of unique chars
         elif set(dict1.keys()) != set(dict2.keys()):
             return False
         return True
