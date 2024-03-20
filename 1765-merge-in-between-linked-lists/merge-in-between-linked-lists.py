d7294eaf500e80e3fcb23db769ee82bd2938eea7
Will Davis <45891395+will-r-davis@users.noreply.github.com>
2024-03-20 03:19:21 -0400

    Time: 188 ms (75.22%) | Memory: 21.1 MB (31.60%) - LeetSync

--- 1765-merge-in-between-linked-lists/merge-in-between-linked-lists.py
+++ 1765-merge-in-between-linked-lists/merge-in-between-linked-lists.py
@@ -22,6 +22,31 @@ class Solution:
         nodeBeforeA.next = head2
         return head1
 
+    
+    """
+    Traversing linked lists typically leads to reference vs. copy issues.
+    Avoiding such issues while maintaining efficiency can lead to code that is somewhat difficult to read.
+    I wanted to pin the following code (not my own) which solves the problem while maintaining clarity 
+    and readability by creating two well-named subfunctions.
+    """
 
+# class Solution:
+#     def mergeInBetween(self, list1: ListNode, a: int, b: int, list2: ListNode) -> ListNode:
+#         def getTail(tail):
+#             while tail and tail.next:
+#                 tail = tail.next
+#             return tail
 
+#         def getNodesAtIndex(head, k1, k2):
+#             first = second = head
+#             for i in range(k2):
+#                 if i == k1: first = second
+#                 second = second.next
+#             return first, second
+        
+#         node1, node2 = getNodesAtIndex(list1, a - 1, b)
+#         list2Tail = getTail(list2)
+#         node1.next, list2Tail.next = list2, node2.next
+
+#         return list1
         
